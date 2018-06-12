8541f12305eae2e494d92ae7643a081d72be732a
Bkg112mp <39085460+Bkg112mp@users.noreply.github.com>
2018-06-13 00:50:51 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 var express = require("express")
 var app = express()
 var qs = require("querystring")
-var port=3000;
+const port=3000;
 var server = http.createServer(app)
 var socketio = require("socket.io")
 
@@ -11,28 +11,60 @@ app.use(express.static('static'))
 
 var path = require("path")
 
+server.listen(port, function() {
+  console.log("serwer startuje na porcie 3000")
+});
+
 app.get("/", function (req, res) {
     console.log(__dirname)
     res.sendFile(path.join(__dirname + "/static/index.html"))
-
 })
 
-app.listen(port, function() {
-  console.log("serwer startuje na porcie 3000")
+var io = socketio.listen(server) 
 
-});
+io.sockets.on("connection", function (client) {    
+    console.log("klient się podłączył"+ client.id) 
+    // client.id - unikalna nazwa klienta generowana przez socket.io
+})
 
 function user(id, name, color) {
   this.id = id;
   this.name = name;
   this.color = color;
 }
+
 var tabUsers = [];
 var color;
- var io = socketio.listen(server)
-io.sockets.on("connection", function (client) {
+ io.sockets.on("connection", function (client) {
+    console.log("klient się podłączył " + client.id)
+     io.sockets.to(client.id).emit("start", {
+        id: client.id,
+    }) 
 
-    if(tabUsers.length == 0) color = "red"
+    client.on("addUser", function(data){
+        if(tabUsers.length == 0){
+            tabUsers.push(new user(data.id, data.nick, "red"))
+            io.sockets.to(data.id).emit("color", {
+                name:data.nick,
+                color: "red"
+            })
+        } else{
+            tabUsers.push(new user(data.id, data.nick, "blue"))
+            io.sockets.to(data.id).emit("color", {
+                name:data.nick,
+                color: "blue"
+            })
+        }
+    })
+    
+    client.on("update", function(data){
+        
+        io.sockets.emit("updated", {
+            action: data
+        })
+        console.log(data)
+    })
+   /*  if(tabUsers.length == 0) color = "red"
     else color = "blue"
 
     tabUsers.push(new user(client.id, "test", ))
@@ -51,8 +83,5 @@ io.sockets.on("connection", function (client) {
         client.emit("onconnect", {
             stan: "play",
         })
-    }
-
-
-
+    }  */
 })
